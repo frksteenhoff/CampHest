72ea6002c9539f7c0ea088ef3f95f95c08999ab4
Henriette Steenhoff <henriette.steenhoff@gmail.com>
2019-02-11 17:06:33 +0100

    Aded new page, hjælp

---
+++ src/components/Help.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Vi gør din Roskilde endnu bedre</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis dui dapibus, sagittis neque id, rhoncus augue. Curabitur iaculis cursus ipsum ac fringilla. Curabitur eu lorem ut ligula volutpat viverra in in elit. Fusce malesuada ultrices augue a iaculis. Aliquam varius, eros eu ornare vestibulum, tortor mauris molestie orci, venenatis egestas felis lorem ut sem. Aenean a dui condimentum, efficitur nisi malesuada, sodales purus. Aenean in mi et sem cursus gravida. Nam id tortor nec ligula dignissim ultricies. Vestibulum venenatis eget dolor sodales mollis. Ut porta et risus vitae elementum. Vivamus sodales lorem elit, iaculis ornare enim semper in. </p>
+
+    <img class="imglong" src="../assets/heste_fest/hest_maddie.jpg">
+
+   <!-- List all prior events -->
+    <div class="topics" v-for="item in items" v-bind:key="item.id"
+         :to="`${item.no}`">
+      <h1>{{ item.header }}</h1>
+      <p>{{ item.desc }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Help',
+  data() {
+    return {
+      items: [
+        {
+          id: 0,
+          header: "How-to-telt",
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis dui dapibus, sagittis neque id, rhoncus augue. Curabitur iaculis cursus ipsum ac fringilla. Curabitur eu lorem ut ligula volutpat viverra in in elit. Fusce malesuada ultrices augue a iaculis. Aliquam varius, eros eu ornare vestibulum, tortor mauris molestie orci, venenatis egestas felis lorem ut sem. Aenean a dui condimentum, efficitur nisi malesuada, sodales purus. Aenean in mi et sem cursus gravida. Nam id tortor nec ligula dignissim ultricies. Vestibulum venenatis eget dolor sodales mollis. Ut porta et risus vitae elementum. Vivamus sodales lorem elit, iaculis ornare enim semper in.'
+          },
+        {
+          id: 1,
+          header: "How-to pavillion",
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis dui dapibus, sagittis neque id, rhoncus augue. Curabitur iaculis cursus ipsum ac fringilla. Curabitur eu lorem ut ligula volutpat viverra in in elit. Fusce malesuada ultrices augue a iaculis. Aliquam varius, eros eu ornare vestibulum, tortor mauris molestie orci, venenatis egestas felis lorem ut sem. Aenean a dui condimentum, efficitur nisi malesuada, sodales purus. Aenean in mi et sem cursus gravida. Nam id tortor nec ligula dignissim ultricies. Vestibulum venenatis eget dolor sodales mollis. Ut porta et risus vitae elementum. Vivamus sodales lorem elit, iaculis ornare enim semper in.'          
+        },      ]
+    }
+  }
+}
+</script>
+<style>
+
+</style>
